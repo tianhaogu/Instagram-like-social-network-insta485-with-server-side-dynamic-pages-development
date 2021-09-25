23ca9bc1bb70490c8ff6086e7646c296bfa313ac
Tianhao Gu <gtianhao@umich.edu>
2021-09-25 17:24:34 -0400

    fix post

--- insta485/views/posts.py
+++ insta485/views/posts.py
@@ -63,3 +63,54 @@ def show_post(postid_url_slug):
     context['comments'] = comment_result.fetchall()
 
     return flask.render_template("post.html", **context)
+
+
+@insta485.app.route('/posts/', methods=["POST"])
+def operate_post():
+    logname = flask.session["logname"]
+    operation_value = request.form["operation"]
+    connection = insta485.model.get_db()
+
+    if operation_value == "delete":
+        postid_value = request.form["postid"]
+
+        post_result = connection.execute(
+            "SELECT postid, owner, filename FROM posts "
+            "WHERE postid = ?", (postid_value,)
+        )
+        curr_post = post_result.fetchone()
+
+        path = insta485.app.config["UPLOAD_FOLDER"]/curr_post["filename"]
+        with open(path, 'r') as handle_file:
+            os.remove(path)
+
+        if curr_post["owner"] != logname:
+            flask.abort(403)
+        connection.execute(
+            "DELETE FROM posts WHERE postid = ?", (postid_value)
+        )
+    elif operation_value == "create":
+        fileobj = flask.request.files["file"]
+        if fileobj is None:
+            flask.abort(400)
+        filename = fileobj.filename
+
+        uuid_basename = "{stem}{suffix}".format(
+            stem=uuid.uuid4().hex,
+            suffix=pathlib.Path(filename).suffix
+        )
+        path = insta485.app.config["UPLOAD_FOLDER"]/uuid_basename
+        fileobj.save(path)
+
+        connection.execute(
+            "INSERT INTO posts(filename, owner) VALUES "
+            "(?, ?)", (filename, logname)
+        )
+    else:
+        pass
+
+    if (request.args.get("target", "notarget") == "notarget" 
+            or request.args.get("target") is None):
+        return flask.redirect(flask.url_for('show_user', user_url_slug=logname))
+    else:
+        return flask.redirect(request.args.get("target"))
