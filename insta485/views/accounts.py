79c9e31bd5ec2133974e522398b5bed25d595c74
Tianhao Gu <gtianhao@umich.edu>
2021-09-25 22:47:09 -0400

    remove print

--- insta485/views/accounts.py
+++ insta485/views/accounts.py
@@ -238,8 +238,6 @@ def operate_accounts():
         salt = uuid.uuid4().hex
         hash_obj = hashlib.new(algorithm)
         password_salted = salt + newpassword1
-        print("salt1: ", salt1)
-        print("newpassword1: ", newpassword1)
         hash_obj.update(password_salted.encode('utf-8'))
         password_hash = hash_obj.hexdigest()
         password_db_string = "$".join([algorithm, salt, password_hash])
